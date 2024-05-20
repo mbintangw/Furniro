73c89546680a22502273310dad5ea6525b947fbb
mbintangw <mbintangw@gmail.com>
2024-05-20 17:41:30 +0700

    add: Category components and add data in data.json

---
+++ src/Components/Home/Category.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import chair from '../../image/chair/rigel.jpg'
+import sofa from '../../image/sofa/procyon.jpg'
+import table from '../../image/table/vortex.jpg'
+import wardrobe from '../../image/wardrobe/tegar.jpg'
+import lamp from '../../image/lamp/lumos.jpg'
+
+const Category = () => {
+  return (
+    <div className='flex flex-col justify-center items-center mt-14 gap-[62px]'>
+      <div>
+        <h1 className='text-center'>Browse The Range</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+      </div>
+      <div className='grid grid-cols-3 gap-5 text-center'>
+        <div className='card-category'>
+          <img src={chair} alt="chair"/>
+          <h2>Chair</h2>
+        </div>
+        <div className='card-category'>
+          <img src={sofa} alt="sofa" />
+          <h2 >Sofa</h2>
+        </div>
+        <div className='card-category'>
+          <img src={table} alt="table" className='!object-fill'/>
+          <h2 >Table</h2>
+        </div>
+        <div className='card-category'>
+          <img src={wardrobe} alt="wardrobe" />
+          <h2 >Wardrobe</h2>
+        </div>
+        <div className='card-category'>
+          <img src={lamp} alt="lamp" className='object-left'/>
+          <h2 >Lamp</h2>
+        </div>
+        <div className='card-category'>
+          <img src={chair} alt="chair" />
+          <h2 >Chair</h2>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default Category
--- src/Pages/HomePages.jsx
+++ src/Pages/HomePages.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from '../Components/Home/HeroSection'
+import Category from '../Components/Home/Category'
 
 const HomePages = () => {
   return (
     <div>
       <HeroSection/>
+      <Category/>
     </div>
   )
 }
