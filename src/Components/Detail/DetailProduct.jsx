b1729b69188d08e67f809d28e0ac968a195202c9
mbintangw <mbintangw@gmail.com>
2024-06-21 13:24:18 +0700

    feat : detail product

--- src/Components/Detail/DetailProduct.jsx
+++ src/Components/Detail/DetailProduct.jsx
@@ -1,26 +1,162 @@
-import React, { useState, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { FaStar, FaStarHalf, FaFacebook, FaLinkedin } from 'react-icons/fa'
+import { AiFillTwitterCircle } from "react-icons/ai";
 import data from '../../../data.json'
 
 const DetailProduct = () => {
-  const { id } = useParams();
-  const [product, setProduct] = useState(null);
+  const { id } = useParams()
+  const [product, setProduct] = useState(null)
+  const [selectedSize, setSelectedSize] = useState(null)
+  const [selectedColor, setSelectedColor] = useState(null)
 
   useEffect(() => {
-    const foundProduct = data.find((item) => item.id === parseInt(id, 10));
-    setProduct(foundProduct);
-  }, [id]);
+    const foundProduct = data.find((item) => item.id === parseInt(id, 10))
+    setProduct(foundProduct)
+  }, [id])
 
   if (!product) {
-    return <div>Loading...</div>;
+    return <div>Loading...</div>
+  }
+
+  const renderStars = (rating) => {
+    const stars = []
+    const fullStars = Math.floor(rating)
+    const hasHalfStar = rating % 1 !== 0
+
+    // Full stars
+    for (let i = 0; i < fullStars; i++) {
+      stars.push(<FaStar key={i} className='text-yellow-400' />)
+    }
+
+    // Half star
+    if (hasHalfStar) {
+      stars.push(<FaStarHalf key='half' className='text-yellow-400' />)
+    }
+
+    // Empty stars
+    const emptyStarsCount = 5 - stars.length // Assuming a maximum of 5 stars
+    for (let i = 0; i < emptyStarsCount; i++) {
+      stars.push(
+        <AiOutlineStar key={`empty-${i}`} className='text-yellow-400' />
+      )
+    }
+
+    return stars
+  }
+
+  const handleSizeClick = (size) => {
+    setSelectedSize(size === selectedSize ? null : size)
+  }
+  const handleColorClick = (color) => {
+    setSelectedColor(color === selectedColor ? null : color)
   }
 
   return (
-    <div>
-      <h1>{product.name}</h1>
-      <img src={product.image} alt={product.name} />
-      <p>{product['additional-information']}</p>
-      <h4>Rp. {product.price}</h4>
+    <div className='py-9'>
+      <div className='flex flex-row justify-center gap-20 '>
+        <div className='flex  gap-8'>
+          <div className='flex flex-col gap-4'>
+            <img
+              src={product.image}
+              alt={product.name}
+              className='w-[76px] h-[80px] rounded-xl'
+            />
+            <img
+              src={product.image}
+              alt={product.name}
+              className='w-[76px] h-[80px] rounded-xl'
+            />
+            <img
+              src={product.image}
+              alt={product.name}
+              className='w-[76px] h-[80px] rounded-xl'
+            />
+            <img
+              src={product.image}
+              alt={product.name}
+              className='w-[76px] h-[80px] rounded-xl'
+            />
+          </div>
+          <div>
+            <img
+              src={product.image}
+              alt={product.name}
+              className='w-[423px] h-[500px] rounded-xl'
+            />
+          </div>
+        </div>
+
+        <div className='flex flex-col gap-5'>
+          <h1>{product.name}</h1>
+          <h4>Rp. {product.price}</h4>
+          <div className='text-yellow-400 flex items-center'>
+            {renderStars(product.rating)}
+            <p>({product.rating})</p>
+          </div>
+          <p>{product['additional-information']}</p>
+
+          <h5 className='text-md font-semibold mb-2 text-[#9F9F9F]'>Size</h5>
+          <div className='flex flex-row gap-2'>
+            {product.size &&
+              product.size.length > 0 &&
+              product.size.map((size, index) => (
+                <button
+                  key={index}
+                  className={`py-1 px-3 rounded ${selectedSize === size ? 'bg-primary text-white' : 'bg-[#F9F1E7] text-black'}`}
+                  onClick={() => handleSizeClick(size)}
+                >
+                  {size}
+                </button>
+              ))}
+          </div>
+
+          <h5 className='text-md font-semibold mb-2 text-[#9F9F9F]'>Color</h5>
+          <div className='flex gap-2'>
+            {product.color &&
+              product.color.length > 0 &&
+              product.color.map((color, index) => (
+                <button
+                  key={index}
+                  className={`text-white py-2 px-2 rounded-full w-8 h-8 border-2 ${selectedColor === color ? `border-4 border-primary` : 'border-transparent'}`}
+                  style={{ backgroundColor: color }}
+                  onClick={() => handleColorClick(color)}
+                ></button>
+              ))}
+          </div>
+
+          <div className='flex gap-3 mt-8 pb-10'>
+            <button className='outline  !text-black rounded-2xl py-[17px] px-12'>
+              Add to Chart
+            </button>
+            <button className='outline  !text-black rounded-2xl py-[17px] px-12'>
+              Add to Chart
+            </button>
+            <button className='outline  !text-black rounded-2xl py-[17px] px-12'>
+              + Compare
+            </button>
+          </div>
+
+          <div className='border-t-2 pt-10'>
+            <table>
+              <tbody className='text-[#9F9F9F]'>
+                <tr >
+                  <td >Category</td>
+                  <td className='pl-2'><span className='text-[#9F9F9F]'> : </span> {product.category}</td>
+                </tr>
+                <tr>
+                  <td>Tags</td>
+                  <td className='pl-2'><span className='text-[#9F9F9F]'> : </span>{product.tags.join(', ')}</td>
+                </tr>
+                <tr>
+                  <td>Share </td>
+                  <td className='pl-2 flex items-center gap-3 text-black'><span className='text-[#9F9F9F] -mr-2'> : </span> <FaFacebook/> <FaLinkedin /> <AiFillTwitterCircle /> </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- src/Components/Shop/FilterShop.jsx
+++ src/Components/Shop/FilterShop.jsx
@@ -1,44 +1,58 @@
-import React, { useState }  from 'react'
+import React, { useState } from 'react'
 import filter from '../../image/icon/filter.svg'
 import grid from '../../image/icon/grid-big-round.svg'
 import list from '../../image/icon/view-list.svg'
 
-const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategories, setSelectedCategories, filteredProducts}) => {
+const FilterShop = ({
+  setViewMode,
+  setItemsPerPage,
+  setSortOrder,
+  selectedCategories,
+  setSelectedCategories,
+  filteredProducts,
+}) => {
   const [isfilterOpen, setIsfilterOpen] = useState(false)
 
   const handleSortOrderChange = (event) => {
-    setSortOrder(event.target.value);
+    setSortOrder(event.target.value)
   }
 
   const handleItemsPerPageChange = (event) => {
-    setItemsPerPage(event.target.value);
+    setItemsPerPage(event.target.value)
   }
 
   const handleCategoryChange = (event) => {
-    const { value, checked } = event.target;
+    const { value, checked } = event.target
 
     if (value === 'all') {
-      setSelectedCategories(checked ? ['all'] : []);
+      setSelectedCategories(checked ? ['all'] : [])
     } else {
       setSelectedCategories((prevCategories) => {
         if (checked) {
-          return prevCategories.includes('all') 
-            ? [value] 
-            : [...prevCategories, value];
+          return prevCategories.includes('all')
+            ? [value]
+            : [...prevCategories, value]
         } else {
-          const newCategories = prevCategories.filter((category) => category !== value);
-          return newCategories.length === 0 ? ['all'] : newCategories;
+          const newCategories = prevCategories.filter(
+            (category) => category !== value
+          )
+          return newCategories.length === 0 ? ['all'] : newCategories
         }
-      });
+      })
     }
-  };
+  }
 
   return (
     <div className='relative'>
       <div className='bg-[#F9F1E7] w-full h-[100px] flex justify-between items-center px-6 lg:px-[100px]'>
         <div className='flex flex-row gap-6 lg:items-center lg:justify-center h-[37px]'>
           <div className='relative flex gap-2'>
-            <img src={filter} className='w-7 h-7 cursor-pointer' alt='filter' onClick={() => setIsfilterOpen(!isfilterOpen)} />
+            <img
+              src={filter}
+              className='w-7 h-7 cursor-pointer'
+              alt='filter'
+              onClick={() => setIsfilterOpen(!isfilterOpen)}
+            />
             <h4 className='font-normal ml-2 '>Filter</h4>
             {isfilterOpen && (
               <div className='absolute mt-10 bg-white border rounded shadow-lg p-4 z-30 w-[150px]'>
@@ -48,7 +62,9 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
                     <input
                       type='checkbox'
                       value='all'
-                      checked={selectedCategories && selectedCategories.includes('all')}
+                      checked={
+                        selectedCategories && selectedCategories.includes('all')
+                      }
                       onChange={handleCategoryChange}
                     />
                     All
@@ -59,7 +75,10 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
                     <input
                       type='checkbox'
                       value='chair'
-                      checked={selectedCategories && selectedCategories.includes('chair')}
+                      checked={
+                        selectedCategories &&
+                        selectedCategories.includes('chair')
+                      }
                       onChange={handleCategoryChange}
                     />
                     Chair
@@ -70,7 +89,10 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
                     <input
                       type='checkbox'
                       value='sofa'
-                      checked={selectedCategories && selectedCategories.includes('sofa')}
+                      checked={
+                        selectedCategories &&
+                        selectedCategories.includes('sofa')
+                      }
                       onChange={handleCategoryChange}
                     />
                     Sofa
@@ -81,7 +103,10 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
                     <input
                       type='checkbox'
                       value='lamp'
-                      checked={selectedCategories && selectedCategories.includes('lamp')}
+                      checked={
+                        selectedCategories &&
+                        selectedCategories.includes('lamp')
+                      }
                       onChange={handleCategoryChange}
                     />
                     Lamp
@@ -92,7 +117,10 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
                     <input
                       type='checkbox'
                       value='table'
-                      checked={selectedCategories && selectedCategories.includes('table')}
+                      checked={
+                        selectedCategories &&
+                        selectedCategories.includes('table')
+                      }
                       onChange={handleCategoryChange}
                     />
                     Table
@@ -103,7 +131,10 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
                     <input
                       type='checkbox'
                       value='vase'
-                      checked={selectedCategories && selectedCategories.includes('vase')}
+                      checked={
+                        selectedCategories &&
+                        selectedCategories.includes('vase')
+                      }
                       onChange={handleCategoryChange}
                     />
                     Vase
@@ -114,7 +145,10 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
                     <input
                       type='checkbox'
                       value='wardrobe'
-                      checked={selectedCategories && selectedCategories.includes('wardrobe')}
+                      checked={
+                        selectedCategories &&
+                        selectedCategories.includes('wardrobe')
+                      }
                       onChange={handleCategoryChange}
                     />
                     Wardrobe
@@ -124,8 +158,18 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
             )}
           </div>
           <div className='flex flex-row gap-6'>
-            <img src={grid} className='w-7 h-7 cursor-pointer' alt='grid' onClick={() => setViewMode('grid')} />
-            <img src={list} className='w-7 h-7 cursor-pointer' alt='list' onClick={() => setViewMode('list')} />
+            <img
+              src={grid}
+              className='w-7 h-7 cursor-pointer'
+              alt='grid'
+              onClick={() => setViewMode('grid')}
+            />
+            <img
+              src={list}
+              className='w-7 h-7 cursor-pointer'
+              alt='list'
+              onClick={() => setViewMode('list')}
+            />
           </div>
           <h4 className='font-normal border-l-4 pl-4 border-[#9F9F9F] h-full lg:flex items-center hidden'>
             Showing 1 of {filteredProducts} results
@@ -136,24 +180,26 @@ const FilterShop = ({setViewMode, setItemsPerPage, setSortOrder, selectedCategor
           <div className='lg:flex flex-row gap-3 hidden'>
             <h4 className='font-normal'>Show</h4>
             <select onChange={handleItemsPerPageChange} className='ml-2'>
-              <option value="10">10</option>
-              <option value="30">30</option>
-              <option value="all">All</option>
+              <option value='10'>10</option>
+              <option value='30'>30</option>
+              <option value='all'>All</option>
             </select>
           </div>
           <div className='flex flex-row gap-3'>
             <h4 className='font-normal'>Sort by</h4>
-            <select onChange={handleSortOrderChange} className='ml-2 w-5 lg:w-max'>
-              <option value="default" >Default</option>
-              <option value="asc">Price: Low to High</option>
-              <option value="desc">Price: High to Low</option>
+            <select
+              onChange={handleSortOrderChange}
+              className='ml-2 w-5 lg:w-max'
+            >
+              <option value='default'>Default</option>
+              <option value='asc'>Price: Low to High</option>
+              <option value='desc'>Price: High to Low</option>
             </select>
           </div>
         </div>
       </div>
     </div>
-  );
-
+  )
 }
 
 export default FilterShop
