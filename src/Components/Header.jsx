283a6194fc98e30dfdb7ac8d493bca67eff660cb
mbintangw <mbintangw@gmail.com>
2024-06-25 14:47:46 +0700

    style : edit Navlink when active

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false)
 
   const toggleMenu = () => {
-    setMenuOpen(!menuOpen);
+    setMenuOpen(!menuOpen)
   }
 
   return (
@@ -19,33 +19,120 @@ const Header = () => {
       <div className='flex flex-row justify-between container mx-auto items-center h-[80px] '>
         <h1 className='text-[32px]'> Furniro. </h1>
         <nav className='hidden lg:flex lg:flex-row w-[430px] justify-between h-6'>
-          <NavLink to='/' className="hover:border-b-4 hover:border-primary ">Home</NavLink>
-          <NavLink to='/Shop' className="hover:border-b-4 hover:border-primary ">Shop</NavLink>
-          <NavLink to='/About' className="hover:border-b-4 hover:border-primary ">About</NavLink>
-          <NavLink to='/Contact' className="hover:border-b-4 hover:border-primary ">Contact</NavLink>
+          <NavLink
+            to='/'
+            className={({ isActive}) =>
+             isActive ? 'border-b-4 border-primary' : ''
+            }
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to='/Shop'
+            className={({ isActive}) =>
+              isActive ? 'border-b-4 border-primary' : ''
+             }
+          >
+            Shop
+          </NavLink>
+          <NavLink
+            to='/About'
+            className={({ isActive}) =>
+              isActive ? 'border-b-4 border-primary' : ''
+             }
+          >
+            About
+          </NavLink>
+          <NavLink
+            to='/Contact'
+            className={({ isActive}) =>
+              isActive ? 'border-b-4 border-primary' : ''
+             }
+          >
+            Contact
+          </NavLink>
         </nav>
         <div className='hidden lg:flex lg:flex-row gap-11'>
-          <img src={account}/>
-          <img src={search}/>
-          <img src={heart}/>
-          <img src={cart}/>
+            <NavLink to='/Account'>
+              <img src={account} alt='account' />
+            </NavLink>
+              
+            <NavLink to='/Search'>
+              <img src={search} alt='search' />
+            </NavLink>
+            
+            <NavLink to='/Favorite'>
+              <img src={heart} alt='heart' />
+            </NavLink>
+            
+            <NavLink to='/Cart'>
+              <img src={cart} alt='cart' />
+            </NavLink>
         </div>
-        <img src={menuOpen ? close : list} alt='menu' className='lg:hidden h-10' onClick={toggleMenu} />
+        <img
+          src={menuOpen ? close : list}
+          alt='menu'
+          className='lg:hidden h-10'
+          onClick={toggleMenu}
+        />
       </div>
-      
+
       {menuOpen && (
         <div className='flex flex-col lg:hidden bg-white/80 absolute top-[80px] left-0 w-full'>
           <nav className='flex flex-col '>
-            <NavLink to='/' onClick={toggleMenu} className='py-2 px-5 border-b hover:bg-white'>Home</NavLink>
-            <NavLink to='/Shop' onClick={toggleMenu} className='py-2 px-5 border-b hover:bg-white'>Shop</NavLink>
-            <NavLink to='/About' onClick={toggleMenu} className='py-2 px-5 border-b hover:bg-white'>About</NavLink>
-            <NavLink to='/Contact' onClick={toggleMenu} className='py-2 px-5 hover:bg-white'>Contact</NavLink>
+            <NavLink
+              to='/'
+              onClick={toggleMenu}
+              className={({ isActive}) =>
+                isActive ? 'py-2 px-5 bg-primary font-bold' : 'py-2 px-5'
+               }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to='/Shop'
+              onClick={toggleMenu}
+              className={({ isActive}) =>
+                isActive ? 'py-2 px-5 bg-primary font-bold' : 'py-2 px-5'
+               }
+            >
+              Shop
+            </NavLink>
+            <NavLink
+              to='/About'
+              onClick={toggleMenu}
+              className={({ isActive}) =>
+                isActive ? 'py-2 px-5 bg-primary font-bold' : 'py-2 px-5'
+               }
+            >
+              About
+            </NavLink>
+            <NavLink
+              to='/Contact'
+              onClick={toggleMenu}
+              className={({ isActive}) =>
+                isActive ? 'py-2 px-5 bg-primary font-bold' : 'py-2 px-5'
+               }
+            >
+              Contact
+            </NavLink>
           </nav>
           <div className='flex flex-row gap-11 justify-center py-5'>
-            <img src={account} alt='account' />
-            <img src={search} alt='search' />
-            <img src={heart} alt='heart' />
-            <img src={cart} alt='cart' />
+            <NavLink to='/Account'>
+              <img src={account} alt='account' />
+            </NavLink>
+              
+            <NavLink to='/Search'>
+              <img src={search} alt='search' />
+            </NavLink>
+            
+            <NavLink to='/Favorite'>
+              <img src={heart} alt='heart' />
+            </NavLink>
+            
+            <NavLink to='/Cart'>
+              <img src={cart} alt='cart' />
+            </NavLink>
           </div>
         </div>
       )}
