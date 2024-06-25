783e6db8872a5c3da860b07309e1cf15acd05d17
mbintangw <mbintangw@gmail.com>
2024-06-25 14:50:58 +0700

    feat : link to shop page

--- src/Components/Home/HeroSection.jsx
+++ src/Components/Home/HeroSection.jsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import background from '../../image/home-furniture.jpg'
+import { Link } from 'react-router-dom'
 
 const HeroSection = () => {
   return (
     <div className='w-full lg:h-screen'>
-      <img src={background} className='w-full h-full'/>
+      <img src={background} className='w-full h-full' />
       <div className='lg:card-arrival rounded-xl p-11 bg-[#FFF3E3] '>
         <h5 className='font-semibold'>New Arrival</h5>
-        <h1 className='text-[52px] text-primary'>Discover Our New Collection</h1>
-        <p className='font-medium'>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod, maiores.</p>
-        <button className='fill mt-[46px]'>BUY NOW</button>
+        <h1 className='text-[52px] text-primary'>
+          Discover Our New Collection
+        </h1>
+        <p className='font-medium'>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod,
+          maiores.
+        </p>
+        <Link to='/Shop'>
+          <button className='fill mt-[46px]'>BUY NOW</button>
+        </Link>
       </div>
     </div>
   )
--- src/Components/Home/OurProducts.jsx
+++ src/Components/Home/OurProducts.jsx
@@ -1,47 +1,56 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import data from '../../../data.json'
 import share from '../../image/icon/share-white.svg'
 import like from '../../image/icon/heart-white.svg'
 import compare from '../../image/icon/compare-white.svg'
 
-
 const OurProducts = () => {
-  
-
-  let product = [];
-  product.push (data[0],data[11],data[21],data[31],data[41],data[51],data[45],data[24])
+  let product = []
+  product.push(
+    data[0],
+    data[11],
+    data[21],
+    data[31],
+    data[41],
+    data[51],
+    data[45],
+    data[24]
+  )
 
   return (
     <div className='flex flex-col justify-center items-center gap-8'>
       <h1 className='text-[40px]'>Our Products</h1>
       <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
-        {product.map((item,index) => (
+        {product.map((item, index) => (
           <Link to={`/Detail/${item.id}`} key={index}>
-          <div className='card-product' key={index}>
-            <img src={item.image}/>
-            <h3>{item.name}</h3>
-            <p className='font-medium text-gray-500 line-clamp-1'>{item['additional-information']}</p>
-            <h4>Rp. {item.price}</h4>
-            <div className='overlay'>
-              <div className='flex flex-col items-center gap-6'>
-                <button className='outline outline-none'>Add to Chart</button>
-                <div className='flex flex-row text-white justify-center items-center gap-[10px]'>
-                  <img src={share} alt='like'/>
-                  <p>Share</p>
-                  <img src={compare} alt='like'/>
-                  <p>Compare</p>
-                  <img src={like} alt='like'/>
-                  <p>Like</p>  
+            <div className='card-product' key={index}>
+              <img src={item.image} />
+              <h3>{item.name}</h3>
+              <p className='font-medium text-gray-500 line-clamp-1'>
+                {item['additional-information']}
+              </p>
+              <h4>Rp. {item.price}</h4>
+              <div className='overlay'>
+                <div className='flex flex-col items-center gap-6'>
+                  <button className='outline outline-none'>Add to Chart</button>
+                  <div className='flex flex-row text-white justify-center items-center gap-[10px]'>
+                    <img src={share} alt='like' />
+                    <p>Share</p>
+                    <img src={compare} alt='like' />
+                    <p>Compare</p>
+                    <img src={like} alt='like' />
+                    <p>Like</p>
+                  </div>
                 </div>
               </div>
-
             </div>
-          </div>
           </Link>
         ))}
       </div>
+      <Link to='/Shop'>
         <button className='outline'>Show More</button>
+      </Link>
     </div>
   )
 }
