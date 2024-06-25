c7a4618bfd742279c20e8964c089afc7eef1e8db
mbintangw <mbintangw@gmail.com>
2024-06-25 11:42:14 +0700

    feat : add RelatedProduct

---
+++ src/Components/Detail/RelatedProduct.jsx
@@ -0,0 +1,54 @@
+import React, {useState} from 'react'
+import {Link} from 'react-router-dom'
+import data from '../../../data.json'
+import share from '../../image/icon/share-white.svg'
+import like from '../../image/icon/heart-white.svg'
+import compare from '../../image/icon/compare-white.svg'
+
+const RelatedProduct = () => {
+  const [showMore, setShowMore] = useState(false)
+  const moreproducts = showMore ? data : data.slice(0, 4)
+
+  return (
+  <div className='flex flex-col items-center justify-center gap-11'>
+      <div >
+        <h1 className='font-normal'>Related Products</h1>
+      </div>
+
+      <div className='grid grid-cols-4 gap-8'>
+        {moreproducts.length > 0 ? (moreproducts.map((item,index) => (
+          <Link to={`/Detail/${item.id}`} key={index}>
+          <div className='card-product' key={index}>
+            <img src={item.image}/>
+            <h3>{item.name}</h3>
+            <p className='font-medium text-gray-500 line-clamp-1'>{item.additional_information}</p>
+            <h4>Rp. {item.price}</h4>
+            <div className='overlay'>
+              <div className='flex flex-col items-center gap-6'>
+                <button className='outline outline-none'>Add to Chart</button>
+                <div className='flex flex-row text-white justify-center items-center gap-[10px]'>
+                  <img src={share} alt='like'/>
+                  <p>Share</p>
+                  <img src={compare} alt='like'/>
+                  <p>Compare</p>
+                  <img src={like} alt='like'/>
+                  <p>Like</p>  
+                </div>
+              </div>
+
+            </div>
+          </div>
+          </Link>
+        ))
+        ) : (
+          <p>No more products</p>)}
+      </div>
+
+      {data.length > 4 && (
+        <button className='outline' onClick={() => setShowMore(!showMore)}>{showMore ? 'Show Less' : 'Show More'}</button>
+      )}
+    </div>
+  )
+}
+
+export default RelatedProduct
--- src/Pages/DetailPages.jsx
+++ src/Pages/DetailPages.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '../Components/Detail/Navbar'
 import DetailProduct from '../Components/Detail/DetailProduct'
 import DescriptionProduct from '../Components/Detail/DescriptionProduct'
+import RelatedProduct from '../Components/Detail/RelatedProduct'
 
 const DetailPages = () => {
   return (
-    <div>
+    <div className='flex flex-col gap-[56px]'>
       <Navbar/>
       <DetailProduct/>
       <DescriptionProduct/>
+      <RelatedProduct />
     </div>
   )
 }
